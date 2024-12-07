8f2d8b73f7cd2c66431b57c6c448c118437bdbb6
John Sarmiento <johnsarmiento117@gmail.com>
2024-12-07 13:53:57 +0800

    Added navbar, and events

--- src/components/ProtectedPage.jsx
+++ src/components/ProtectedPage.jsx
@@ -1,6 +1,6 @@
 // ProtectedPage.js
 import React from 'react';
-import { useState, useRef } from "react"
+import { useState, useEffect, useRef } from "react"
 import { motion } from 'motion/react';
 import "../styles.css"
 
@@ -8,6 +8,10 @@ import names from '../assets/Names White.png'
 import bridesmaids from "../assets/JeffThrysha-Bridesmaids.png"
 import groomsmen from "../assets/JeffThrysha-Groomsmen.png"
 import scallop from "../assets/Scallop2.png"
+import ceremonySched from "../assets/Ceremony.png"
+import cocktailsSched from "../assets/Cocktails.png"
+import drinksSched from "../assets/Drinks.png"
+import receptionSched from "../assets/Reception.png"
 
 const faq = [
     {
@@ -58,10 +62,10 @@ const ProtectedPage = () => {
         if (selected == i) {
             return setSelected(null);
         }
-
         setSelected(i);
     }
 
+    //For sending RSVP form data to Google Sheets
     const formRef = useRef(null)
     const scriptUrl = "https://script.google.com/macros/s/AKfycbyJPjl_Sk02igUqY-gyTU2uv6X04aNQgnn7tvhkSg38KOK2KPuhDmco8Meelh5oYh0b/exec"
     const [loading, setLoading] = useState(false)
@@ -85,23 +89,68 @@ const ProtectedPage = () => {
     <>
         {/* This div is for the first part of the site you will see */}
         <div className="landingPage">
+            {/* Navbar */}
+            <nav class="navbar navbar-expand-lg navbar-dark bg-transparent p-4 m-2">
+                <div class="container">
+                <a class="navbar-brand" href="#"></a>
+                    {/* Hamburger Button */}
+                    <button class="navbar-toggler shadow-none border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    {/* Sidebar */}
+                    <div class="sidebar offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    
+                        {/* Sidebar Header */}
+                        <div class="offcanvas-header text-white">
+                            <button type="button" class="btn-close btn-close-white shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+
+                        {/* Sidebar Body */}
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 navbarLinks">
+                            <li class="nav-item mx-3">
+                                <a class="nav-link active" aria-current="page" href="#">home</a>
+                            </li>
+                            <li class="nav-item mx-3">
+                                <a class="nav-link" href="#details">details</a>
+                            </li>
+                            <li class="nav-item mx-3">
+                                <a class="nav-link" href="#attire">attire</a>
+                            </li>
+                            <li class="nav-item mx-3">
+                                <a class="nav-link" href="#schedule">schedule</a>
+                            </li>
+                            <li class="nav-item mx-3">
+                                <a class="nav-link" href="#faq">faq</a>
+                            </li>
+                            <li class="nav-item mx-3">
+                                <a class="nav-link" href="#photos">photos</a>
+                            </li>
+                            </ul>
+                            <a class="btn custom-btn" href="#rsvp" role="button">RSVP</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
             <motion.div className="landingCenter">
-            <motion.p
-                style={{fontSize: '1rem', color: '#FBF6EE', marginBottom: '-1.2%'}}
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                exit={{opacity: 0}}
-                transition={{ duration: .3}}>
-                    welcome to the wedding of
-            </motion.p>
-            <motion.img 
-                src={names}
-                style={{width: '25em', height: 'auto', marginTop: '-2em', marginBottom: '-1.5em'}} 
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                exit={{opacity: 0}}
-                transition={{ duration: 1, type: 'tween'}}>
-            </motion.img>
+                <motion.p
+                    style={{fontSize: '1rem', color: '#FBF6EE', marginBottom: '-1.2%'}}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    transition={{ duration: .3}}>
+                        welcome to the wedding of
+                </motion.p>
+                <motion.img 
+                    src={names}
+                    style={{width: '25%', marginTop: '-2em', marginBottom: '-1.5em'}} 
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    transition={{ duration: 1, type: 'tween'}}>
+                </motion.img>
                 <h2 style={{fontSize: '1.2rem', fontWeight: '310', color: '#FBF6EE'}}>03 08 2025</h2>
             </motion.div>
         </div>
@@ -112,7 +161,7 @@ const ProtectedPage = () => {
         {/* This div contains the main body of the website */}
         <div className="mainPage">
             {/* Wedding Details */}
-            <div className="detailsPage">
+            <div className="detailsPage" id='details'>
                 <motion.h1 style={{color: '#0047bb'}}>details for the big day!</motion.h1>
                 <div className="locationDetails">
                     <h2 style={{color: '#f2533f', fontSize: '2vw', margin: '0'}}>CEREMONY</h2>
@@ -136,16 +185,22 @@ const ProtectedPage = () => {
             </div>
 
             {/* Schedule of Events */}
-            <motion.div>
+            <motion.div id='schedule' className='schedPage'>
                 <h1 style={{color: '#0047bb'}}>schedule of events</h1>
+                <motion.div className='schedRow'>
+                    <motion.img src={ceremonySched}></motion.img>
+                    <motion.img src={cocktailsSched}></motion.img>
+                    <motion.img src={receptionSched}></motion.img>
+                    <motion.img src={drinksSched}></motion.img>
+                </motion.div>
             </motion.div>
 
             {/* Attire Details */}
-            <div className="attirePage">
+            <div className="attirePage" id='attire'>
                 <motion.h1 style={{color: '#0047BB'}}>what to wear</motion.h1>
                 <div className="attireRow">
                 <div className="attireClass">
-                    <img src={groomsmen} style={{width: '30em', height: 'auto', marginTop: '-5em', marginBottom: '-5em'}} />
+                    <img src={groomsmen} style={{width: '50%', marginTop: '-4em', marginBottom: '-4em'}} />
                     <div className='attireDescription'>
                         <h2 className="subHeading" style={{color: '#0047BB', margin: '0'}}>FOR THE GENTLEMEN:</h2>
                         <a style={{margin: '0'}}>Barong Tagalog</a>
@@ -155,7 +210,7 @@ const ProtectedPage = () => {
                 </div>
                 <div className="attireRow">
                 <div className="attireClass">
-                    <img src={bridesmaids} style={{width: '30em', height: 'auto', marginTop: '-5em', marginBottom: '-5em'}} />
+                    <img src={bridesmaids} style={{width: '50%', marginTop: '-4em', marginBottom: '-4em'}} />
                     <div className='attireDescription'>
                         <h2 className="subHeading" style={{color: '#9A0051', margin: '0'}}>FOR THE LADIES:</h2>
                         <a style={{color: '#9A0051', margin: '0'}}>Formal Dress</a>
@@ -169,21 +224,8 @@ const ProtectedPage = () => {
             </motion.img>
 
             {/* FAQ Segment */}
-            <div className="faqPage">
+            <div className="faqPage" id='faq'>
                 <h1 className="heading" style={{color: '#0047BB'}}>FAQs</h1>
-                {/* <div className="accordion">
-                    {faq.map((item, i) => (
-                        <div className='item'>
-                            <div className='title' onClick={() => toggle(i)}>
-                                <h2>{item.question}</h2>
-                                <span>{selected === i ? '-' : '+'}</span>
-                            </div>
-                            <div className={selected === i ? 'content show' : 'content'}>
-                                <p>{item.answer}</p>
-                            </div>
-                        </div>
-                    ))}
-                </div> */}
                 <div class="accordion accordion-flush" id="accordionFlushExample">
                     <div class="accordion-item">
                         <h2 class="accordion-header">
@@ -293,6 +335,7 @@ const ProtectedPage = () => {
 
             {/* This div contains the RSVP form segment */}
             <motion.div
+            id='rsvp'
             className="rsvpPage"
             initial={{opacity: 0}}
             whileInView={{
@@ -348,7 +391,7 @@ const ProtectedPage = () => {
                 </section>
             </motion.div>
 
-            <motion.div className='photoGallery'>
+            <motion.div className='photoGallery' id='photos'>
                 <h1 style={{color: '#0047BB'}}>have a look at our prenup!</h1>
             </motion.div>
         </div>
